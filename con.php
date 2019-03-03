5cc9f017505f8bd098f282253cc13ceeabd0764f
EC2 Default User <ec2-user@ip-172-31-6-86.us-east-2.compute.internal>
2019-03-03 22:21:17 +0000

    third commit

--- con.php
+++ con.php
@@ -11,8 +11,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
--- register.php
+++ register.php
@@ -1,1 +1,37 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "GroupMeet";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$first_name = $_POST['fname'];
+$last_name = $_POST['lname'];
+$email = $_POST['Email'];
+$password = $_POST['Password'];
+$password_confirm = $_POST['Confirm'];
+
+$sql = "INSERT INTO Users (first_name, last_name, email, password, password_confirm) VALUES ('$first_name', '$last_name', '$email', '$password', '$password_confirm')";
+
+echo $first_name;
+echo $last_name;
+echo $email;
+echo $password;
+echo $password_confirm;
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+// https://www.w3schools.com/php/php_mysql_insert.asp
+?> 
+
